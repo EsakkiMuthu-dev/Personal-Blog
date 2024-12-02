27a3fa26e98ca0568864eb359295f0c685ed6c4f
Esakki Muthu <esakkimuthu6842@gmail.com>
2024-12-02 21:28:15 +0530

    My Personal blog Setup

--- src/config.ts
+++ src/config.ts
@@ -1,27 +1,27 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
-  ogImage: "astropaper-og.jpg",
+  website: "https://your-blog-domain.com/", // Replace with your deployed domain
+  author: "Esakki Muthu",
+  profile: "https://esakkimuthu-portfolio.vercel.app/",
+  desc: "A minimal, responsive, and SEO-friendly Astro blog theme to share learnings and act as a journal.",
+  title: "Esakki's Blog",
+  ogImage: "esakki-blog-og.jpg", // Replace with your custom OG image
   lightAndDarkMode: true,
   postPerIndex: 4,
   postPerPage: 3,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   editPost: {
-    url: "https://github.com/satnaing/astro-paper/edit/main/src/content/blog",
+    url: "https://github.com/EsakkiMuthu-dev/astro-paper/edit/main/src/content/blog",
     text: "Suggest Changes",
     appendFilePath: true,
   },
 };
 
 export const LOCALE = {
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+  lang: "en", // html lang code
+  langTag: ["en-EN"], // BCP 47 Language Tags
 } as const;
 
 export const LOGO_IMAGE = {
@@ -34,122 +34,20 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: ` ${SITE.title} on Github`,
-    active: true,
-  },
-  {
-    name: "Facebook",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Facebook`,
-    active: true,
-  },
-  {
-    name: "Instagram",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Instagram`,
+    href: "https://github.com/EsakkiMuthu-dev",
+    linkTitle: `${SITE.title} on Github`,
     active: true,
   },
   {
     name: "LinkedIn",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://www.linkedin.com/in/esakkimuthu-esakki/",
     linkTitle: `${SITE.title} on LinkedIn`,
     active: true,
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:esakkimuthu6842@gmail.com", // Replace with your email
     linkTitle: `Send an email to ${SITE.title}`,
-    active: false,
-  },
-  {
-    name: "X",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on X`,
-    active: false,
-  },
-  {
-    name: "Twitch",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Twitch`,
-    active: false,
-  },
-  {
-    name: "YouTube",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on YouTube`,
-    active: false,
-  },
-  {
-    name: "WhatsApp",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on WhatsApp`,
-    active: false,
-  },
-  {
-    name: "Snapchat",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Snapchat`,
-    active: false,
-  },
-  {
-    name: "Pinterest",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Pinterest`,
-    active: false,
-  },
-  {
-    name: "TikTok",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on TikTok`,
-    active: false,
-  },
-  {
-    name: "CodePen",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on CodePen`,
-    active: false,
-  },
-  {
-    name: "Discord",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Discord`,
-    active: false,
-  },
-  {
-    name: "GitLab",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on GitLab`,
-    active: false,
-  },
-  {
-    name: "Reddit",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Reddit`,
-    active: false,
-  },
-  {
-    name: "Skype",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Skype`,
-    active: false,
-  },
-  {
-    name: "Steam",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Steam`,
-    active: false,
-  },
-  {
-    name: "Telegram",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Telegram`,
-    active: false,
-  },
-  {
-    name: "Mastodon",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Mastodon`,
-    active: false,
-  },
+    active: true,
+  }
 ];
--- src/pages/about.md
+++ src/pages/about.md
@@ -3,34 +3,101 @@ layout: ../layouts/AboutLayout.astro
 title: "About"
 ---
 
-AstroPaper is a minimal, responsive and SEO-friendly Astro blog theme. I designed and crafted this based on [my personal blog](https://satnaing.dev/blog).
+Hi, I'm **Esakki Muthu** 👋!  
+A **Backend Developer at Zoho** specializing in backend development using **Java**, and currently expanding my expertise in **Rust**.
 
-This theme is aimed to be accessible out of the box. Light and dark mode are supported by
-default and additional color schemes can also be configured.
+I also have experience with **React**, **Node.js**, and **Python**. My curiosity drives me to explore the inner workings of systems, constantly asking *"why?"* to better understand how things work under the hood.  
 
-This theme is self-documented \_ which means articles/posts in this theme can also be considered as documentations. So, see the documentation for more info.
+### What I'm passionate about:
+- Building efficient and scalable systems.
+- Exploring new programming paradigms.
+- Writing clean, maintainable, and effective code.
+
+For more details, feel free to check out my [portfolio](https://esakkimuthu-portfolio.vercel.app/) or [GitHub](https://github.com/EsakkiMuthu-dev).
+
+---
 
 <div>
   <img src="/assets/dev.svg" class="sm:w-1/2 mx-auto" alt="coding dev illustration">
 </div>
 
-## Tech Stack
+## My Tech Stack
 
-This theme is written in vanilla JavaScript (+ TypeScript for type checking) and a little bit of ReactJS for some interactions. TailwindCSS is used for styling; and Markdown is used for blog contents.
+Here’s a quick overview of the tools and technologies I work with:
 
-## Features
+### Backend
+- **Java**, **Node.js**, **Python**, **Rust**, **Spring Boot**
 
-Here are certain features of this site.
+### Databases
+- **PostgreSQL**, **MySQL**, **MongoDB**, **SQLite**
 
-- fully responsive and accessible
-- SEO-friendly
-- light & dark mode
-- fuzzy search
-- super fast performance
-- draft posts
-- pagination
-- sitemap & rss feed
-- highly customizable
+### DevOps
+- **Linux**, **Google Cloud**, **GitHub Actions**
 
-If you like this theme, you can star/contribute to the [repo](https://github.com/satnaing/astro-paper).  
-Or you can even give any feedback via my [email](mailto:contact@satnaing.dev).
+### Frontend
+- **React**, **HTML**, **CSS**, **JavaScript**
+
+### Tools
+- **Postman**, **VS Code**, **GitHub**, **Docker**, **IntelliJ**, **Firebase**
+
+### Architectures
+- **MVC**, **Microservices**, **Serverless**, **SPA**
+
+---
+
+### A Fun Introduction in Code 🧑‍💻
+
+```rust
+println!("👋 Hello there! Let’s meet Esakki Muthu!");
+
+struct EsakkiMuthu {
+    username: &'static str,
+    name: &'static str,
+    position: &'static str,
+    blog: &'static str,
+    socials: Socials,
+    code: Code,
+    architecture: [&'static str; 4],
+}
+
+struct Socials {
+    github: &'static str,
+    linkedin: &'static str,
+}
+
+struct Code {
+    backend: [&'static str; 5],
+    database: [&'static str; 4],
+    devops: [&'static str; 3],
+    frontend: [&'static str; 4],
+    tools: [&'static str; 6],
+}
+
+fn main() {
+    let me = EsakkiMuthu {
+        username: "esakkimuthu",
+        name: "Esakki Muthu",
+        position: "Backend Developer at Zoho",
+        blog: "https://esakkimuthu-portfolio.vercel.app/",
+        socials: Socials {
+            github: "https://github.com/EsakkiMuthu-dev",
+            linkedin: "https://www.linkedin.com/in/esakkimuthu-esakki/",
+        },
+        code: Code {
+            backend: ["Java", "Node.js", "Python", "Rust", "Spring Boot"],
+            database: ["PostgreSQL", "MySQL", "MongoDB", "SQLite"],
+            devops: ["Linux", "Google Cloud", "GitHub Actions"],
+            frontend: ["React", "HTML", "CSS", "JavaScript"],
+            tools: ["Postman", "VS Code", "GitHub", "Docker", "IntelliJ", "Firebase"],
+        },
+        architecture: ["MVC", "Microservices", "Serverless", "SPA"],
+    };
+
+    println!(
+        "{} is a {} who codes in {} and builds {}!",
+        me.name,
+        me.position,
+        me.code.backend.join(", "),
+        me.architecture.join(" and ")
+    );
+}
